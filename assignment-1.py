51092f879f22c62bf6334e1192e9468fa75e8c2b
Shyamal kar <shyamalkar430@gmail.com>
2026-02-28 21:23:45 +0530

    Update assignment-1.py

--- assignment-1.py
+++ assignment-1.py
@@ -3,7 +3,7 @@ Problem Statement: Write a Python program that does the following:
 1.  Takes two numbers as input from the user.
 2.  Performs the basic mathematical operations on these two numbers:
  Addition
- Subtraction
+ Subtraction 
  Multiplication
  Division
 3.  Displays the results of each operation on the screen.
