401a0ee13051c14e8e3b1cd581ccda5bdcfe49bf
Shyamal kar <shyamalkar430@gmail.com>
2026-03-21 22:04:59 +0530

    area_of_triangle.py

--- area_of_triangle.py
+++ area_of_triangle.py
@@ -3,7 +3,6 @@ When all the length of the sides of the triangel is known - a, b, c
 Semi perimeter (s) = (a + b + c) / 2
 Area = square root of (s * (s - a) * (s - b) * (s - c))
 """
-
 a = float(input("Enter first side: "))
 b = float(input("Enter second side: "))
 c = float(input("Enter third side: "))
