6bb1aacc85894471dd46c9af2329e40b30b2ec78
Shyamal kar <shyamalkar430@gmail.com>
2026-06-14 22:35:22 +0530

    Update input_function.py

--- Basic_python/input_function.py
+++ Basic_python/input_function.py
@@ -13,7 +13,7 @@ print(type(age)) # always return as a string .
 #Then how to work with string ?
 #With int() or float()  type conversion .
 age = int(input("Enter your age: "))
-print(age + 5) # Because here add + 5 
+print(age + 5) # Because here add + 5  
 
 # Float input 
 
